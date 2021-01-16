18311a227064762426ab1a9f0e3e711d9a3c903d
majewskimateusz <kontakt@mateuszmajewski.com>
2021-01-16 22:45:00 +0100

    Realizacja tematu Stałe i annotacje

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,6 +2,9 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
+
+	public static final int RETURN = -1;
+
 	Node last;
 	int i;
 
@@ -25,13 +28,13 @@ public class IntLinkedList {
 
 	public int top() {
 		if (isEmpty())
-			return -1;
+			return RETURN;
 		return last.value;
 	}
 
 	public int pop() {
 		if (isEmpty())
-			return -1;
+			return RETURN;
 		int ret = last.value;
 		last = last.prev;
 		return ret;
